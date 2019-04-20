b29ff1dae34ed9075eaec4dbf05b494249e7e73a
Robert MBP Huie <rhuie@digitalpixies.com>
2019-04-20 15:46:28 -0700

    version 3.4.1

---
+++ make.sh
@@ -0,0 +1,4 @@
+cp dist/css/bootstrap.css dist/css/bootstrap.css.less
+./node_modules/less/bin/lessc dist/css/nestable-bootstrap.less dist/css/nestable-bootstrap.css
+cp dist/css/bootstrap-theme.css dist/css/bootstrap-theme.css.less
+./node_modules/less/bin/lessc dist/css/nestable-bootstrap-theme.less dist/css/nestable-bootstrap-theme.css
